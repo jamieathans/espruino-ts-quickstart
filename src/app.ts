4b5dead6b6f4c4a2ac871a4bcebe668e21f52027
James Athanassakis <jamie_athans@hotmail.com>
2025-09-06 20:02:54 +0930

    onInit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import appConfig from "./app-config";
 
-let on = false;
-setInterval(() => {
-  on = !on;
-  LED1.write(on);
-}, appConfig.blinkPeriodMilliSec);
+//let on = false;
+//setInterval(() => {
+//  on = !on;
+//  LED1.write(on);
+//}, appConfig.blinkPeriodMilliSec);
+
+function onInit() {
+  const temp = E.getTemperature();
+  console.log(`Temp = ${temp}`);
+  setInterval(() => {
+    LED1.toggle();
+  }, appConfig.blinkPeriodMilliSec);
+}
