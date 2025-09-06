4975de34c13ebc0c8c43e85654e742cf7b68a0d9
James Athanassakis <jamie_athans@hotmail.com>
2025-09-06 12:41:16 +0930

    Fixes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import appConfig = require("./app-config");
+import appConfig from "./app-config";
 
+let on = false;
 setInterval(() => {
-    const temp = E.getTemperature();
-    console.log("Current temp:", temp);
-}, appConfig.default.refreshPeriodSec * 1000);
-
+  on = !on;
+  LED1.write(on);
+}, appConfig.blinkPeriodMilliSec);
---
+++ wifi.d.ts
@@ -0,0 +1,2 @@
+declare var LED1: Pin;
+declare var LED2: Pin;
